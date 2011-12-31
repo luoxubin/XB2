c8a0e4991706881b2e23525ed73a644f7fb75a6a
luoxubin <luoxubin@luoxubins-MacBook-Pro.local>
2011-12-31 20:59:17 +0800

    Initial Commit

---
+++ XB/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XB
+//
+//  Created by luoxubin on 12/31/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
